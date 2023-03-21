47af1923839af2ae28643c8f485fee323cca0012
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-03-21 17:01:49 +0300

    draft: failed hadoop

--- bd-iot/own_build/build_images.sh
+++ bd-iot/own_build/build_images.sh
@@ -3,5 +3,3 @@ set -e
 #cp ~/.ssh/id* base/
 docker build -t owuor/hdfs-base:latest ./hadoop
 docker build -t owuor/hdfs-namenode:latest ./namenode
-docker build -t owuor/zeppelin-build:latest ./zeppelin/build
-docker build -t owuor/zeppelin:latest ./zeppelin/image
