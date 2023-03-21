fcf59f2be38f2812f5ea1f6d6e81c8d4d5289234
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-03-21 06:25:36 +0300

    zeppelin draft: failed because of too high memory

--- bd-iot/own_build/build_images.sh
+++ bd-iot/own_build/build_images.sh
@@ -3,4 +3,5 @@ set -e
 #cp ~/.ssh/id* base/
 docker build -t owuor/hdfs-base:latest ./hadoop
 docker build -t owuor/hdfs-namenode:latest ./namenode
-docker build -t owuor/zeppelin:latest ./zeppelin
+docker build -t owuor/zeppelin-build:latest ./zeppelin/build
+docker build -t owuor/zeppelin:latest ./zeppelin/image
