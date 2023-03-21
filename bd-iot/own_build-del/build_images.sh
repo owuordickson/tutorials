cefc43f7bef11e4f3c9146732fb8bdea6c88c664
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-03-21 19:26:46 +0300

    removed and updated

--- bd-iot/own_build-del/build_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-#cp ~/.ssh/id* base/
-docker build -t owuor/hdfs-base:latest ./hadoop
-docker build -t owuor/hdfs-namenode:latest ./namenode
--- bd-iot/own_build-del/namenode/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-[ ! -d "/hadoop/data/nn" ] && /hadoop/bin/hdfs --config /etc/hadoop namenode -format 
-/hadoop/sbin/start-dfs.sh
