4933b3fad1ba3610b0c76db352767a362b760439
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-04-03 14:26:09 +0300

    fixed hadoop image bug

--- bd-iot/own_build/hadoop/build_images.sh
+++ bd-iot/own_build/hadoop/build_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 #cp ~/.ssh/id* base/
-docker build -t owuor/hdfs-base:latest ./base
-docker build -t owuor/hdfs-namenode:latest ./namenode
-docker build -t owuor/hdfs-datanode:latest ./datanode
+docker build -t owuordickson/hdfs-base:latest ./base
+docker build -t owuordickson/hdfs-namenode:latest ./namenode
+docker build -t owuordickson/hdfs-datanode:latest ./datanode
