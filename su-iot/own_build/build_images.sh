6eedb7ba5e1969fb9ef127f936f3b9f073c304f7
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-05-05 19:46:49 +0300

    updated

--- su-iot/own_build/build_images.sh
+++ su-iot/own_build/build_images.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 #cp ~/.ssh/id* base/
-docker build -t owuordickson/fritzing:fedora ./fritzing
-
-
+docker build -t owuordickson/fritzing:ubuntu ./fritzing
 
 # https://l10nn.medium.com/running-x11-applications-with-docker-75133178d090
