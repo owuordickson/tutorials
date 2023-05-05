ccfc51828cd2e9285246e99028cdf35febe5e5bc
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-05-05 19:42:20 +0300

    Fedora does not work (removed)

--- su-iot/own_build/fritzing/build-linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-
-mkdir build
-docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic qmake ../phoenix.pro
-docker run -v "$(pwd):/home/conan/fritzing" -w /home/conan/fritzing/build fritzing/build:bionic make -j16
--- su-iot/own_build/fritzing/xvfb-release-helper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -xe
-
-xvfb-run ./release.sh "$1"
