452f222fcb334058f5fa4f58915752b616449385
Dickson Owuor (Ph.D.) <owuordickson@gmail.com>
2023-05-10 20:57:27 +0300

    updated

--- su-iot/own_build/build_images.sh
+++ su-iot/own_build/build_images.sh
@@ -2,3 +2,5 @@
 set -e
 #cp ~/.ssh/id* base/
 docker build -t owuordickson/fritzing:ubuntu ./fritzing
+
+# docker run -ti --rm -e DISPLAY=host.docker.internal:0 --name fritzing owuordickson/fritzing:ubuntu
